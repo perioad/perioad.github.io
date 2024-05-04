d5e4d4b0867c98d6482e04c74df962ac32be37f1
Aleksandr Perio <aleksandr_perio@epam.com>
2024-05-04 17:07:44 +0300

    feat: change build step to github actions

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Hello world 2</h1>
+      <h1>Hello world 3</h1>
     </main>
   );
 }
