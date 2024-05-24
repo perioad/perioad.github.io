840b9c22a1b9b847380b270c9662056512869e11
Aleksandr Perio <aleksandr_perio@epam.com>
2024-05-24 18:40:42 +0300

    feat: add swoosh sound to tracks in list

--- src/components/audio-player-tracks/AudioPlayerTracks.tsx
+++ src/components/audio-player-tracks/AudioPlayerTracks.tsx
@@ -1,4 +1,5 @@
 import { FC } from 'react';
+import { useAudioEffect } from '../../hooks/useAudioEffect';
 
 type Props = {
   tracks: string[];
@@ -11,6 +12,12 @@ export const AudioPlayerTracks: FC<Props> = ({
   indexActive,
   selectTrack,
 }) => {
+  const swooshSound = useAudioEffect('audio/swoosh.mp3');
+
+  function handleHover() {
+    swooshSound.current?.play();
+  }
+
   return (
     <ul className="max-h-32 overflow-scroll border border-b-0 border-t-0 border-pink-500 sm:max-h-36">
       {tracks.map((track, index) => (
@@ -18,6 +25,7 @@ export const AudioPlayerTracks: FC<Props> = ({
           <button
             className={`${index === indexActive ? 'bg-pink-500' : ''} group w-full border-b border-pink-500 px-5 py-1 group-first:border-t`}
             onClick={() => selectTrack(index)}
+            onMouseEnter={handleHover}
           >
             <span className="inline-block transition-all group-hover:scale-125">
               {track}
