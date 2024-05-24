1d357b9162cd309afa14993d9cdf5840b9abb253
Aleksandr Perio <aleksandr_perio@epam.com>
2024-05-24 18:38:10 +0300

    fix: add border top to first track in list

--- src/components/audio-player-tracks/AudioPlayerTracks.tsx
+++ src/components/audio-player-tracks/AudioPlayerTracks.tsx
@@ -14,9 +14,9 @@ export const AudioPlayerTracks: FC<Props> = ({
   return (
     <ul className="max-h-32 overflow-scroll border border-b-0 border-t-0 border-pink-500 sm:max-h-36">
       {tracks.map((track, index) => (
-        <li key={track}>
+        <li className="group" key={track}>
           <button
-            className={`${index === indexActive ? 'bg-pink-500' : ''} group w-full border-b border-pink-500 px-5 py-1`}
+            className={`${index === indexActive ? 'bg-pink-500' : ''} group w-full border-b border-pink-500 px-5 py-1 group-first:border-t`}
             onClick={() => selectTrack(index)}
           >
             <span className="inline-block transition-all group-hover:scale-125">
